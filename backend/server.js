e72557dddb94cf19b4c2b234b82b97d086a8ff5d
vikkkyvignesh <tcarts2020to2023@gmail.com>
2025-05-07 14:23:52 +0530

    fourth

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
