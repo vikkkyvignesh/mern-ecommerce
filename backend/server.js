a32cb8088c7dd31c11a8d8a29919b6e97a3486bb
vikkkyvignesh <tcarts2020to2023@gmail.com>
2025-05-14 14:40:01 +0530

    12

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 // import authRoutes from "./routes/auth.route.js";
 // import productRoutes from "./routes/product.route.js";
 // import cartRoutes from "./routes/cart.route.js";
