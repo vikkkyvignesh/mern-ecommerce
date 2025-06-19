5aad3a4cc59b23aaf2f1f912f524303e14e1179b
vikkkyvignesh <tcarts2020to2023@gmail.com>
2025-06-19 13:29:11 +0530

    10

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -71,23 +71,23 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
     if (user && (await user.comparePassword(password))) {
       const { accessToken, refreshToken } = generateTokens(user._id);
       await storeRefreshToken(user._id, refreshToken);
       setCookies(res, accessToken, refreshToken);
-      res.status(200).json({
+
+      res.json({
         _id: user._id,
         name: user.name,
         email: user.email,
         role: user.role,
-        message: "Login successful",
       });
     } else {
-      res.status(401).json({ message: "Invalid credentials" });
+      res.status(400).json({ message: "Invalid email or password" });
     }
   } catch (error) {
-    console.log("Error logging in", error.message);
-
+    console.log("Error in login controller", error.message);
     res.status(500).json({ message: error.message });
   }
 };
@@ -118,14 +118,16 @@ export const refreshToken = async (req, res) => {
     const refreshToken = req.cookies.refreshToken;
 
     if (!refreshToken) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "No refresh token provided" });
     }
 
     const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
     const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
-    if (refreshToken !== storedToken) {
-      return res.status(401).json({ message: "Unauthorized" });
+
+    if (storedToken !== refreshToken) {
+      return res.status(401).json({ message: "Invalid refresh token" });
     }
+
     const accessToken = jwt.sign(
       { userId: decoded.userId },
       process.env.ACCESS_TOKEN_SECRET,
@@ -133,16 +135,16 @@ export const refreshToken = async (req, res) => {
     );
 
     res.cookie("accessToken", accessToken, {
-      httpOnly: true, //prevent XSS attacks
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict", //prevents CSRF attacks
+      sameSite: "strict",
       maxAge: 15 * 60 * 1000,
     });
 
     res.json({ message: "Token refreshed successfully" });
   } catch (error) {
-    console.log("Error in controller", error.message);
-    res.status(500).json({ message: error.message });
+    console.log("Error in refreshToken controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 };
 
